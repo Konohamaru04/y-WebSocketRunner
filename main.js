62f38efae0176766171267f0699853a2c944d30c
Amartya Deshmukh <dedsec.konohamaru@gmail.com>
2023-05-30 20:53:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-var exec = require('child_process').exec;
+const WebSocket = require('ws')
+const http = require('http')
+const wss = new WebSocket.Server({ noServer: true })
+const setupWSConnection = require('./utils.js').setupWSConnection
 
-exec('npx y-websocket',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 1234
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('okay')
+})
+
+wss.on('connection', setupWSConnection)
+
+server.on('upgrade', (request, socket, head) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = ws => {
+    wss.emit('connection', ws, request)
+  }
+  wss.handleUpgrade(request, socket, head, handleAuth)
+})
+
+server.listen(port, host, () => {
+  console.log(`running at '${host}' on port ${port}`)
+})
